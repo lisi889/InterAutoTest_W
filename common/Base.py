53f8da46d6a989ef6f0c37a71f61344fbd460d63
lisi8 <lisi889@126.com>
2020-03-09 23:19:48 +0800

    'excel'

--- common/Base.py
+++ common/Base.py
@@ -1,5 +1,12 @@
+import re
+
 from config.Conf import ConfigYaml
 from utils.MysqlUtil import Mysql
+import json
+from utils.AssertUtil import AssertUtil
+from utils.LogUtil import my_log
+p_data = re.compile('\${(.*)}\$')
+log = my_log()
 #定义init_db
 def init_db(db_alias):
 # db_1:
@@ -23,5 +30,58 @@ def init_db(db_alias):
     print(conn)
     return conn
 
+def json_parse(data):
+    """格式化字符，转换json"""
+    # 判断header是否存在，json转义
+    # if headers:
+    #     header = json.loads(headers)
+    # else:
+    #     header = headers
+
+    json.loads(data) if data else data
+# 查询方法、公共方法
+def res_find(data,patten_data=p_data):
+    # patten = re.compile('\${(.*)}\$')
+    patten = re.compile(patten_data)
+    re_res = patten.findall(data)
+    return re_res
+#替换
+def res_sub(data,replace,patten_data=p_data):
+    patten = re.compile(patten_data)
+    re_res = patten.findall(data)
+    if re_res:
+        return  re.sub(patten_data,replace,data)
+    return re_res
+
+def assert_db(db_name,result,db_verify):
+    assert_util = AssertUtil()
+    # sql = init_db("db_1")
+    sql = init_db("db_name")
+    # 查询sql，excel定义好的
+    db_res = sql.fechone(db_verify)
+    log.debug("数据库查询结果：{}".format(str(db_res)))
+    # 数据库的结果与接口返回的结果验证
+    # 获取数据库的结果key
+    verify_list = list(dict(db_res).keys())
+    # 根据key获取数据库的结果，接口的结果
+    for line in verify_list:
+        # res_line = res["body"][line]
+        res_line = result[line]
+        res_db_line = dict(db_res)[line]
+    # 验证
+    assert_util.assert_body(res_line, res_db_line)
+
+#验证请求中是否${},返回${}￥内容也作为公共方法
+def params_find(headers,cookies):
+    if "${" in headers:
+        headers = res_find(headers)
+    if "${" in cookies:
+        cookies = res_find(cookies)
+    return headers,cookies
+
+
 if __name__ =="__main__":
-    init_db("db_1")
+    pass
+    # init_db("db_1")
+    # print(res_find('{"Aythorization":"JWT ${token}$"}'))
+    # print(res_sub('{"Aythorization":"JWT ${token}$"}',"123"))
--- common/ExcelData.py
+++ common/ExcelData.py
@@ -5,7 +5,7 @@ class Data:
         #使用ｅｘｃｅｌ工具类，获取结果ｌｉｓｔ
         # self.reder = ExcelReader("../data/testdata.xlsx", "Sheet1")
         self.reder = ExcelReader(tesecase_file,sheet_name)
-        print(self.reder.data())
+        # print(self.reder.data())
 
     def get_run_data(self):
         # 列是否运行内容ｙ
@@ -13,9 +13,35 @@ class Data:
         for line in self.reder.data():
 
             if str(line[DataConfig.is_run]).lower() == "y":
-                print(line)
+                # print(line)
                 #保存执行的结果放到新列表
                 run_list.append(line)
-        print(run_list)
+        # print(run_list)
         #保存要执行的结果，放到新的列表
         return run_list
+    def get_case_list(self):
+        """获取全部的测试用例"""
+        # run_list = list()
+        # for line in self.reder.data():
+        #         # print(line)
+        #         # 保存执行的结果放到新列表
+        #         run_list.append(line)
+                # print(run_list)
+                # 保存要执行的结果，放到新的列表
+
+        #改用列表推到
+        run_list = [line for line in self.reder.data()]
+        return run_list
+    def get_case_pre(self, pre):
+        """获取全部用例
+        根据前置条件从全部中取对应的测试用例"""
+        run_list = self.get_case_list()
+        for line in run_list:
+            if pre in dict(line).values():
+                return line
+        return None
+
+# if __name__ == "__main__":
+#     # ../data/testdata.xlsx Sheet1
+#     # <module 'ntpath' from 'C:\\Users\\lisi8\\AppData\\Local\\Programs\\Python\\Python36\\lib\\ntpath.py'>
+#     Data("../data/testdata.xlsx","Sheet1")
